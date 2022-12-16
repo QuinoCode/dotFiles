6b2c8c5d2518cb27b10bf679c1bd10b8948824ae
Quino <JoaquinAngelValienteRuiz@gmail.com>
2022-12-16 12:30:02 +0100

    Added anoher alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias yay="paru"
 
 #My shortcuts
 alias UPV="/media/quino/Windows/Users/34655/Desktop/UPV/2º/Primer\ cuatrimestre/ "
+alias UPV-CS="/media/quino/Windows/Users/34655/Desktop/Programación/GitHub-repositories/UPV-CS"
 # alias lvim='nvim -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
 
 # get fastest mirrors
