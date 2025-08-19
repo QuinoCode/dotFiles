e8a2c4ed1da355090c62677a951eed0434d05612
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2025-08-19 16:36:07 +0200

    Got my thing done

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -1,4 +1,5 @@
 require('texpresso').attach()
+require('texpresso').texpresso_path = "/home/ayuda106/Desktop/Terpresso/texpresso/build/texpresso"
 local autocmd = vim.api.nvim_create_autocmd
 autocmd({ "InsertLeave" }, {
 	group = vim.api.nvim_create_augroup('texpressoGroup', {}),
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -13,6 +13,8 @@ export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
+## Texpresso
+export PATH="$PATH:$HOME/Desktop/Terpresso/texpresso/build"
 # Bindings
 # Aliases
 ## Movement
