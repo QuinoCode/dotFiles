4f75abc636d7a1db5722ff5bf1ca938fe487868e
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2025-08-19 12:03:40 +0200

    Desperate measures for the all.lua snippets

--- nvim/.config/nvim/lua/luasnip/all.lua
+++ nvim/.config/nvim/lua/luasnip/all.lua
@@ -1,9 +1,11 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
-
+local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
 return {
-  s({ trig = "helloworld", snippetType = "autosnippet", desc = "A hellow world snippet", wordTrig = false },
+  s({ trig = "helloworld", snippetType = "snippet", desc = "A hellow world snippet", wordTrig = true },
     { t("Just a hint of what's to come!"), }
   ),
+  autosnippet("trig", t("expansion")),
+  s({ trig = "man", snippetType = "autosnippet" }, t("ual")),
 }
